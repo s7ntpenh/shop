72b283e070744dcb4a59e220123e4104436458e8
s7ntpenh <denismazur111@gmail.com>
2025-09-02 21:35:37 +0200

    updated base.html, added new parameters to models

--- main/views.py
+++ main/views.py
@@ -7,9 +7,17 @@ def product_list(request, category_slug=None):
 
     category = None
     if category_slug:
-        category = get_object_or_404(Category, category_slug=category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
     return render(request, 'main/product/list.html', {'categoty': category,
                                                       'categories': categories,
                                                       'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    related_products = Product.objects.filter(category=product.category,
+                                              available=True).exclude(id=product.id)[:4]
+    
+    return render(request, 'main/product/detail.html', {'product': product,
+                                                        'related_products': related_products})
